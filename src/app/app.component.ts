8946ee886275e823689cb9665cbc41d0d46281a7
Tu Nombre <gabriel.dehner@rocketlab.ai>
2022-10-26 23:02:56 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'front-docker';
+  responseFromBack: any = 'N/A';
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get('http://localhost:3000/test').subscribe((res) => {
+      this.responseFromBack = res;
+    });
+  }
 }
